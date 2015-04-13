7df3c6b5e3f218c98b3c2126357cc8a7aebd1ef3
Santosh Kumar <santosh79@gmail.com>
2015-04-13 13:01:35 -0400

    md5 hashing the pwd

--- web/models/user.ex
+++ web/models/user.ex
@@ -10,6 +10,9 @@ defmodule ActualEdlabDemo.User do
     timestamps
   end
 
+  before_insert :hash_password
+
+
   @required_fields ~w(first_name last_name email password)
   @optional_fields ~w()
 
@@ -23,4 +26,15 @@ defmodule ActualEdlabDemo.User do
     model
     |> cast(params, @required_fields, @optional_fields)
   end
+
+  def hash_password(changeset) do
+    new_password = changeset.changes.password |> string_to_md5
+    new_changes = %{changeset.changes | password: new_password}
+    %{changeset | changes: new_changes}
+  end
+
+  defp string_to_md5(some_string) do
+    :crypto.hash(:md5, some_string) |> Base.encode64
+  end
+
 end
